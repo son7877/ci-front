624100a38691be7d8bad422a9d50c647d63c46b9
안홍범 <anhongbeom@baaam.local>
2024-05-23 15:10:20 +0900

    board

--- src/config/network.js
+++ src/config/network.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = async (url, method, body) => {
-  axios.defaults.baseURL = "http://localhost:8080/api/v1";
+  axios.defaults.baseURL = "http://34.71.199.63:8080/api/v1";
   const res = await axios({
     url,
     method,
